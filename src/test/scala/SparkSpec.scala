118c3394047af1c54050f62dfbe63eb9f3c4e61d
Porom Sivam Kalita <93148034+PoromSivam@users.noreply.github.com>
2022-04-25 13:08:08 -0400

    Update SparkSpec.scala

--- src/test/scala/SparkSpec.scala
+++ src/test/scala/SparkSpec.scala
@@ -1,1 +1,9 @@
+package edu.neu.ethanalyzer
+
+import com.mypackage.mytasks.IO
+import com.mypackage.mytasks.MyObject.runSparkTransformations
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
